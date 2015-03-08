73dea1ed7212185b635b240529d2f460040c7885
Alexander Steven Rice-Reynolds <ricerea@quest-OptiPlex-9020>
2015-03-08 14:29:23 -0700

    added tar file

---
+++ perflab-handout/config.h
@@ -0,0 +1,32 @@
+/*********************************************************
+ * config.h - Configuration data for the driver.c program.
+ *********************************************************/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 
+ * CPEs for the baseline (naive) version of the rotate function that
+ * was handed out to the students. Rd is the measured CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define R64    2.8
+#define R128   4.1
+#define R256   6.5
+#define R512   10.6
+#define R1024  12.0
+
+/* 
+ * CPEs for the baseline (naive) version of the smooth function that
+ * was handed out to the students. Sd is the measure CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define S32   61.6
+#define S64   61.1
+#define S128  61.1
+#define S256  61.1
+#define S512  61.1
+
+
+#endif /* _CONFIG_H_ */
